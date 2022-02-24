2d1a6c8622d473aeb6fea9cc69ef8e75174c7cd6
Aashish <60545932+aashishjha99@users.noreply.github.com>
2022-02-25 02:26:28 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,9 +3,48 @@ import "./App.css";
 import Login from "./component/Login";
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "*>@CltBdy3BD",
+  };
+
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+
+  const loginCheck = (details) => {
+    console.log(details);
+
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      console.log("Logged In");
+      setUser({
+        email: details.email,
+        password: details.password,
+      });
+    } else {
+      setError("Details not matched");
+    }
+  };
+  const logout = () => {
+    setUser({
+      email: "",
+      password: "",
+    });
+  };
   return (
     <div className="App">
-      <Login />
+      {user.email !== "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.email}</span>
+          </h2>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <Login login={loginCheck} error={error} />
+      )}
     </div>
   );
 }
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,84 +1,53 @@
 import React, { useState } from "react";
-import validator from "validator";
-
 import "./login.css";
 
-function Login() {
-  const initValues = { mailid: "", password: "" };
-  const [formValue, setFormValue] = useState(initValues);
-  const [errormail, seterrormail] = useState("");
-  const [errorpassword, seterrorpassword] = useState("");
+function Login({ login, error }) {
+  const [details, setDetails] = useState({ email: "", password: "" });
 
-  const mailValidator = (value) => {
-    setFormValue(value);
-    // let re =
-    //   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (validator.isEmail(value)) {
-      setFormValue(value);
-      seterrormail("correct mail");
-    } else {
-      seterrormail("not correct mail");
-    }
+  const submit = (e) => {
+    e.preventDefault(); // stop re render
+    login(details);
   };
-  const validate = (value) => {
-    if (
-      validator.isStrongPassword(value, {
-        minLength: 8,
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-        minSymbols: 1,
-      })
-    ) {
-      seterrorpassword("Is Strong Password");
-    } else {
-      seterrorpassword("Is Not Strong Password");
-    }
+
+  const onPassword = (e) => {
+    setDetails({ ...details, password: e.target.value });
   };
+
+  const onmail = (e) => {
+    setDetails({ ...details, email: e.target.value });
+  };
+
   return (
-    <div className="sizing">
-      <form>
-        <h1>Login Form</h1>
-        <div>
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
+    <form onSubmit={submit}>
+      <div className="form-inner ">
+        <h2>Login</h2>
+        {error !== "" ? <div className="error">{error}</div> : ""}
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
           <input
-            type="email"
-            className="form-control col-xs-2"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            name_="mailid"
-            value={formValue.mailid}
-            onChange={(e) => mailValidator(e.target.value)}
-          />
-          <div id="emailHelp" className="form-text">
-            {errormail}
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            name_="password"
-            value={formValue.password}
-            onChange={(e) => validate(e.target.value)}
+            type="text"
+            className="email"
+            id="email"
+            onChange={onmail}
+            value={details.email}
           />
         </div>
-        <div className="mb-3 form-check">
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            {errorpassword}
-          </label>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="text"
+            className="password"
+            id="password"
+            onChange={onPassword}
+            value={details.password}
+          />
         </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+        <label htmlFor="" className="passproperties">
+          Password should be 8- 10 character
+        </label>
+        <input type="submit" value="Login" className="loginbtn" />
+      </div>
+    </form>
   );
 }
 
